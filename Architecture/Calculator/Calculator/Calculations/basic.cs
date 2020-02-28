79ba670d74beaef227cbd3e46c88685c1aec125e
George Filatov <51397432+Liggidarck@users.noreply.github.com>
2020-02-28 17:17:59 +0300

    Add files via upload

---
+++ Architecture/Calculator/Calculator/Calculations/basic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class basic
+    {
+        public void slozheniye(double a, double b)
+        {
+            startMain s = new startMain();
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+            s.startM();
+        }
+
+        public void vychitaniye(double a, double b)
+        {
+            startMain s = new startMain();
+            Console.WriteLine(a + " - " + b + " = " + (a - b));
+            s.startM();
+        }
+
+        public void deleniye(double a, double b)
+        {
+            startMain s = new startMain();
+            Console.WriteLine(a + " / " + b + " = " + (a / b));
+            s.startM();
+        }
+
+        public void umnozheniye(double a, double b)
+        {
+            startMain s = new startMain();
+            Console.WriteLine(a + " * " + b + " = " + (a * b));
+            s.startM();
+        }
+
+
+    }
+}
---
+++ Architecture/Calculator/Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ifstart start = new ifstart();
+            Console.WriteLine("Калькулятор V.0.0.1");
+            start.ifVstart();
+        }
+    }
+}
