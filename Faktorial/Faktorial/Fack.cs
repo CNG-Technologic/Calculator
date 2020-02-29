a9d8379f0cd268234fdb10e4829a38af57694ef3
Fire Owl Haven <61467254+FireOwlHaven@users.noreply.github.com>
2020-02-29 18:48:13 +0300

    Факториал

---
+++ Faktorial/Faktorial/Fack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faktorial
+{
+    class Fack
+    {
+        public void Fukich()
+        {
+            double a;
+            double F = 1;
+            Console.Write("Введите число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            for (; a > 1; a--)
+            {
+                F = F * a;
+            }
+            Console.WriteLine("Факториал  = " + F);
+        }
+    }
+}
