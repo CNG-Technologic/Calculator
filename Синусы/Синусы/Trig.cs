bf62b282ce85b11cd14f3d6436f5833b32ffba0b
Fire Owl Haven <61467254+FireOwlHaven@users.noreply.github.com>
2020-03-01 12:45:29 +0300

    Синусы,Косинусы,Тангенсы,Котангенсы

---
+++ Синусы/Синусы/Trig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Синусы_Косинусы_Тангенсы_Котангенсы
+{
+    class Trig
+    {
+        public void Sinusi()
+        {
+            double a;
+            Console.WriteLine("Введите число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Sin(a / 180D) * Math.PI);
+        }
+        public void Cosinusi()
+        {
+            double a;
+            Console.WriteLine("Введите число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Cos(a / 180D) * Math.PI);
+        }
+        public void Tangensi()
+        {
+            double a;
+            Console.WriteLine("Введите число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Tan(a / 180D) * Math.PI);
+        }
+        public void Cotangensi()
+        {
+            double a;
+            Console.WriteLine("Введите число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Tanh(a / 180D) * Math.PI);
+        }
+    }
+}
