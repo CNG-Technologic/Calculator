a4bdb88ba8d4730d7f5ed9101e44fad2cddf1311
Fire Owl Haven <61467254+FireOwlHaven@users.noreply.github.com>
2020-03-01 13:03:05 +0300

    Степени

---
+++ Степени/Степени/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Степени
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stepeni st = new Stepeni();
+            st.Pow();
+        }
+    }
+}
