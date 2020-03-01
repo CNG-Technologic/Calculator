a5fdb9105f5dc7068a1a93fc361714b729e46b33
Fire Owl Haven <61467254+FireOwlHaven@users.noreply.github.com>
2020-03-01 11:41:39 +0300

    Вычисления сторон прямоугольного треугольника
    Теорема Пифагора

---
+++ SPT/SPT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ris r = new Ris();
+            string v;
+            Console.WriteLine("Чтобы найти гипотенузу по двум катетам введите <1>" +
+                            "\nЧтобы найти катет по гипотенузе и другому катету введите <2>");
+            v = Console.ReadLine();
+            if (v == "1")
+            {
+                r.Gipo();
+            }
+            if (v == "2")
+            {
+                r.Kat();
+            }
+        }
+    }
+}
---
+++ SPT/SPT/Ris.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPT
+{
+    class Ris
+    {
+        public void Gipo()
+        {
+            double a, b, c;
+            Console.Write("Введите первый катет: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второй катет: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            if (a + b > c)
+            {
+                Console.WriteLine("Гипотенуза равна " + c);
+            }
+            else
+            {
+                Console.WriteLine("Такого треугольника не существует");
+            }
+        }
+        public void Kat()
+        {
+            double a, b, c;
+            Console.Write("Введите катет: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите гипотенузу: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            b = Math.Sqrt(Math.Pow(c, 2) - Math.Pow(a, 2));
+            if (a + c > b)
+            {
+                Console.WriteLine("Второй катет равен " + b);
+            }
+            else
+            {
+                Console.WriteLine("Такого треугольника не существует");
+            }
+        }
+    }
+}
