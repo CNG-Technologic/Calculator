f4e958d641d32db9933ab674762f150e9846f2d4
George Filatov <51397432+Liggidarck@users.noreply.github.com>
2020-03-11 17:59:05 +0300

    Calculator_Java [Version 0.0.1]

---
+++ Java/V 0.0.1/Calculator_Java/src/ru/george/start/Main.java
@@ -0,0 +1,12 @@
+package ru.george.start;
+
+import ru.george.start.ifStart;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ifStart st = new ifStart();
+        System.out.println("Калькулятор Java 0.0.1");
+        st.start();
+    }
+}
