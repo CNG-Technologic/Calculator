e5be72fb0a48502d5339d0e8458e034cc9a76e2a
George Filatov <liggidarck@gmail.com>
2021-01-07 18:30:46 +0300

    Renamed folder

---
+++ Math root/Корни/Korni.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Корни
+{
+    class Korni
+    {
+        public void Ki()
+        {
+            double a;
+            Console.WriteLine("Введите число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Корень из " + a + " равен " + Math.Sqrt(a));
+        }
+    }
+}
---
+++ SIN/Синусы/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Синусы_Косинусы_Тангенсы_Котангенсы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trig t = new Trig();
+            string v;
+            Console.WriteLine("Синусы - <1>," +
+                "\nКосинусы - <2>," +
+                "\nТангенсы - <3>," +
+                "\nКотангенсы - <4>");
+            v = Console.ReadLine();
+            if (v == "1")
+            {
+                t.Sinusi();
+            }
+            if (v == "2")
+            {
+                t.Cosinusi();
+            }
+            if (v == "3")
+            {
+                t.Tangensi();
+            }
+            if (v == "4")
+            {
+                t.Cotangensi();
+            }
+            else
+            {
+                Console.WriteLine("Введено некорректное значение");
+            }
+        }
+    }
+}
