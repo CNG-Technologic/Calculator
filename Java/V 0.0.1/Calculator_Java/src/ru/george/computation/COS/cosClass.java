a2c450cfd347458930b0750380ed605992f5c1ea
George Filatov <liggidarck@gmail.com>
2021-01-07 18:25:03 +0300

    C# master branch

--- Java/V 0.0.1/Calculator_Java/src/ru/george/computation/COS/cosClass.java
+++
@@ -1,9 +0,0 @@
-package ru.george.computation.COS;
-
-public class cosClass {
-
-    public void cosVoid(double degrees, double radians) {
-        System.out.format("Косинус %.1f градусов = %.4f%n", degrees, Math.cos(radians));
-    }
-
-}
